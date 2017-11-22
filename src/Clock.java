4826f0459002982f7c7200db5f44e50905725bd8
Taxxon <te15.emil.kack@umea.nti.se>
2017-11-22 13:45:35 +0100

    Klar med NumberDisplay

---
+++ src/Clock.java
@@ -0,0 +1,18 @@
+/**
+ * Created by emka15 on 2017-11-22.
+ */
+public class Clock {
+
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private String displayString;
+
+    public Clock(){
+        hours.setValue(0);
+        minutes.setValue(0);
+    }
+
+    public Clock(int hour, int minute){
+        
+    }
+}
--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -2,4 +2,38 @@
  * Created by Emil Käck on 2017-11-22.
  */
 public class NumberDisplay {
+
+    private int limit;
+    private int value;
+
+    public NumberDisplay(int maxLimit){
+        this.limit = maxLimit;
+        this.value = 0;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if (value >= 0 & value < 60) {
+            this.value = value;
+        }
+    }
+
+    public String getDisplayValue(){
+        if (value < 10){
+            return ("Värde: 0" + value);
+        }else {
+            return ("Värde: " + value);
+        }
+    }
+
+    public void increment(){
+        if (value == 59){
+            value = 0;
+        }else {
+            value = value + 1;
+        }
+    }
 }
